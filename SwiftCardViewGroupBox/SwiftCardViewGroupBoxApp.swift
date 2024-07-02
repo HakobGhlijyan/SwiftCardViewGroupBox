fbb2c5e6ba0797b5d0a1dce065d664adb0ffc5c2
Hakob Ghlijyan <hakobghlijyan@gmail.com>
2024-07-02 12:45:51 +0400

    Initial Commit

---
+++ SwiftCardViewGroupBox/SwiftCardViewGroupBoxApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftCardViewGroupBoxApp.swift
+//  SwiftCardViewGroupBox
+//
+//  Created by Hakob Ghlijyan on 02.07.2024.
+//
+
+import SwiftUI
+
+@main
+struct SwiftCardViewGroupBoxApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
